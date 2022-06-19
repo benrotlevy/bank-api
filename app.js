5a38f9989ebc7311617e075cb9d1bfb4954edcc7
benrotlevy <benjamin@rotlevy.com>
2022-06-19 05:41:49 +0300

    basic api

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 const usersRoute = require("./routes/users-routes.js");
 const manage = require("./routes/manage.js");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "client/build");
+app.use(express.static(publicDirectoryPath));
 
 app.use(cors());
 app.use(express.json());
